7d4d2754775397870a585996e9b2301709335a52
Zakcodes101 <masterstdy@gmail.com>
2026-02-02 10:46:10 +0000

    Setup Appwrite table

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
 endpoint: process.env.XPO_PUBLIC_APPWRITE_ENDPOINT,
 platform: "com.jsm.foodordering",
 projectId: process.env.XPO_PUBLIC_APPWRITE_PROJECT_ID,
+databaseId: "697f43f90034ce0c432a",
+userCollectionId: "69807e9d000b2fae1bb4",
 };
