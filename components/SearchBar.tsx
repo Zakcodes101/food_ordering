a249d7cb81903ad92adb82baa52c3d72cc21e8b7
Zakcodes101 <masterstdy@gmail.com>
2026-02-05 19:55:27 +0000

    refactor searchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,22 +2,24 @@ import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { useState } from "react";
 import { useLocalSearchParams, router } from "expo-router";
 import { images } from "@/constants";
-import { useDebouncedCallback } from "use-debounce";
+
 
 const SearchBar = () => {
   const params = useLocalSearchParams<{ query?: string }>();
   const [query, setQuery] = useState(params.query);
 
-  const debouncedSearch = useDebouncedCallback(
-    (text: string) => router.push(`/search?query=${text}`),
-    500,
-  );
+
 
   const handleSearch = (text: string) => {
     setQuery(text);
-    setQuery(text);
-    debouncedSearch(text);
+    if(!text){
+      router.setParams({ query: undefined });
+    }
   };
+
+  const handleSubmit = () => {
+    if(query?.trim()) router.setParams({query})
+  }
 
   return (
     <View className="searchbar">
@@ -26,11 +28,13 @@ const SearchBar = () => {
         placeholder="Search for pizzas, burgers ..."
         value={query}
         onChangeText={handleSearch}
+        onSubmitEditing={handleSubmit}
         placeholderTextColor="#A0A0A0"
+        returnKeyType="search"
       />
       <TouchableOpacity
         className="pr-5"
-        onPress={() => console.log("search pressed")}
+        onPress={() => router.setParams({query})}
       >
         <Image
           source={images.search}
